2d2dba4b168766d48910bbde3a54ef0241b493cf
Yannick Panis <info@qucode.eu>
2018-01-16 03:05:57 +0100

    add subscription code (broken), change readme to problem log

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -4,6 +4,10 @@ import { graphql } from "react-apollo"
 import gql from "graphql-tag"
 
 class LinkList extends Component {
+  componentDidMount() {
+    this._subscribeToNewLinks()
+    this._subscribeToNewVotes()
+  }
   render() {
     if (this.props.feedQuery && this.props.feedQuery.loading) {
       return <div>Loading...</div>
@@ -34,6 +38,89 @@ class LinkList extends Component {
     votedLink.votes = createVote.link.votes
 
     store.writeQuery({ query: FEED_QUERY, data })
+  }
+  _subscribeToNewLinks = () => {
+    this.props.feedQuery.subscribeToMore({
+      document: gql`
+        subscription {
+          newLink {
+            node {
+              id
+              url
+              description
+              createdAt
+              postedBy {
+                id
+                name
+              }
+              votes {
+                id
+                user {
+                  id
+                }
+              }
+            }
+          }
+        }
+      `,
+      updateQuery: (previous, { subscriptionData }) => {
+        const newAllLinks = [
+          subscriptionData.data.newLink.node,
+          ...previous.feed.links
+        ]
+        const result = {
+          ...previous,
+          feed: {
+            links: newAllLinks
+          }
+        }
+        return result
+      }
+    })
+  }
+  _subscribeToNewVotes = () => {
+    this.props.feedQuery.subscribeToMore({
+      document: gql`
+        subscription {
+          newVote {
+            node {
+              id
+              link {
+                id
+                url
+                description
+                createdAt
+                postedBy {
+                  id
+                  name
+                }
+                votes {
+                  id
+                  user {
+                    id
+                  }
+                }
+              }
+              user {
+                id
+              }
+            }
+          }
+        }
+      `,
+      updateQuery: (previous, { subscriptionData }) => {
+        console.log(`NEW VOTE`)
+        const votedLinkIndex = previous.feed.links.findIndex(
+          link => link.id === subscriptionData.data.newVote.node.link.id
+        )
+        const newAllLinks = previous.feed.links.slice()
+        const result = {
+          ...previous,
+          allLinks: newAllLinks
+        }
+        return result
+      }
+    })
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { BrowserRouter } from "react-router-dom"
-import "./styles/index.css"
-import App from "./components/App"
-import registerServiceWorker from "./registerServiceWorker"
-
-import { ApolloProvider } from "react-apollo"
-import { ApolloClient } from "apollo-client"
 import { HttpLink } from "apollo-link-http"
+import { ApolloClient } from "apollo-client"
+import { ApolloProvider } from "react-apollo"
+import { WebSocketLink } from "apollo-link-ws"
+import { BrowserRouter } from "react-router-dom"
+import { getMainDefinition } from "apollo-utilities"
 import { InMemoryCache } from "apollo-cache-inmemory"
+import { SubscriptionClient } from "subscriptions-transport-ws"
+import { ApolloLink, split } from "apollo-client-preset"
+
+import "./styles/index.css"
+import registerServiceWorker from "./registerServiceWorker"
 import { AUTH_TOKEN } from "./constants"
-import { ApolloLink } from "apollo-client-preset"
+
+import App from "./components/App"
 
 const httpLink = new HttpLink({
   uri: "http://localhost:4000"
@@ -29,8 +33,26 @@ const middlewareAuthLink = new ApolloLink((operation, forward) => {
 
 const httpLinkWithAuthToken = middlewareAuthLink.concat(httpLink)
 
+const subClient = new SubscriptionClient("ws://localhost:4000", {
+  reconnect: true,
+  connectionParams: {
+    authToken: localStorage.getItem(AUTH_TOKEN)
+  }
+})
+
+const wsLink = new WebSocketLink(subClient)
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === "OperationDefinition" && operation === "subscription"
+  },
+  wsLink,
+  httpLinkWithAuthToken
+)
+
 const client = new ApolloClient({
-  link: httpLinkWithAuthToken,
+  link,
   cache: new InMemoryCache()
 })
 
